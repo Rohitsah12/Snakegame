76d7191c27cf0ab906a03be07dfcb51c0bbd65ad
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-12-29 13:18:01 +0530

    Added snake movement logic

--- script.js
+++ script.js
@@ -5,7 +5,49 @@ document.addEventListener('DOMContentLoaded',function (){
     let score=0;
     let gameStarted=false;
     let food={x:300,y:200}; // {x:15*20,y:10*20} // -> cell coordinate -> pixels top left pixels for food
-    let snake = [{x:160,y:200},{x:140,y:200},{x:120,y:200}];
+    let snake = [{x:160,y:200},{x:140,y:200},{x:120,y:200}];//[head,body,body,tail]
+
+    let dx=cellSize; //+20
+    let dy=0;
+
+
+    function updateSnake(){
+        const newHead={x : snake[0].x+dx,y: snake[0].y+dy};
+        snake.unshift(newHead);//Add new head to the snake
+
+        //check collision with food
+        if(newHead.x===food.x && newHead.y===food.y){
+            score+=10;
+            // TODO: move food
+        }
+        else{
+            snake.pop();//remove tail
+        }
+    }
+
+    function changeDirection(e){
+        console.log('Key pressed',e);
+        const isGoingDown=dy===cellSize;
+        const isGoingUp=dy===-cellSize;
+        const isGoingRight=dx===cellSize;
+        const isGoingleft=dx===-cellSize;
+        if(e.key==='Arrowup' && !isGoingDown){
+            dx=0;
+            dy=-cellSize;
+        }else if(e.key==='ArrowDown' && !isGoingUp){
+            dx=0;
+            dy=cellSize;
+        }
+        else if(e.key==='ArrowLeft' && !isGoingRight){
+            dx=-cellSize;
+            dy=0;
+        }
+        else if(e.key==='ArrowRight' && !isGoingleft){
+            dx=cellSize;
+            dy=0;
+        }
+    }
+
 
     function drawDiv(x,y,className){
         const divElement=document.createElement('div');
@@ -30,11 +72,20 @@ document.addEventListener('DOMContentLoaded',function (){
         gameArena.appendChild(foodELement);
     }
 
+
+    function gameLoop(){
+        setInterval(()=>{
+            updateSnake();
+            drawFoodAndSnake();
+        },200);
+    }
+
     function runGame(){
         if(!gameStarted){
             gameStarted=true;
             //gameLoop: todo
-            drawFoodAndSnake();
+            document.addEventListener('keydown',changeDirection);
+            gameLoop();
         }
     }
 
